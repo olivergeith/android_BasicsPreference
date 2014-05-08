2164b7ea2601bff019cea354f655d5fcdbccd8b1
olivergeith <oliver.geith@gmx.net>
2014-05-08 08:42:58 +0200

    Seekbar preference optimized

--- src/de/geithonline/android/basics/preferences/SeekBarPreference.java
+++ src/de/geithonline/android/basics/preferences/SeekBarPreference.java
@@ -9,26 +9,50 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
+/**
+ * @author geith
+ * 
+ *         How to use: Import lib and in preferences xml: <br>
+ * 
+ *         <PreferenceScreen xmlns:android="http://schemas.android.com/apk/res/android"<br>
+ *         xmlns:seekbarpreference="http://schemas.android.com/apk/lib/de.geithonline.android.basics.preferences"><br>
+ * 
+ *         <de.geithonline.android.basics.preferences.SeekBarPreference <br>
+ *         android:key="anzahlPatterns"<br>
+ *         android:title="@string/pattern_anzahlPatterns"<br>
+ *         seekbarpreference:minValue="10"<br>
+ *         seekbarpreference:maxValue="1500"<br>
+ *         android:summary="Draw %1$d Patterns" <br>
+ *         android:defaultValue="1000"<br>
+ *         android:dialogMessage="@string/pattern_anzahlPatterns" /><br>
+ * 
+ * 
+ * 
+ */
 public final class SeekBarPreference extends DialogPreference implements OnSeekBarChangeListener {
 
 	// Namespaces to read attributes
-	private static final String PREFERENCE_NS = "http://schemas.android.com/apk/res/com.mnm.seekbarpreference";
+	// http://schemas.android.com/apk/lib/de.geithonline.android.basics.preferences
+	private static final String PREFERENCE_NS = "http://schemas.android.com/apk/lib/de.geithonline.android.basics.preferences";
 	private static final String ANDROID_NS = "http://schemas.android.com/apk/res/android";
 
 	// Attribute names
 	private static final String ATTR_DEFAULT_VALUE = "defaultValue";
 	private static final String ATTR_MIN_VALUE = "minValue";
 	private static final String ATTR_MAX_VALUE = "maxValue";
+	private static final String ATTR_STEP_VALUE = "stepValue";
 
 	// Default values for defaults
 	private static final int DEFAULT_CURRENT_VALUE = 50;
 	private static final int DEFAULT_MIN_VALUE = 0;
 	private static final int DEFAULT_MAX_VALUE = 100;
+	private static final int DEFAULT_STEP_VALUE = 1;
 
 	// Real defaults
 	private final int mDefaultValue;
 	private final int mMaxValue;
 	private final int mMinValue;
+	private final int mStepValue;
 
 	// Current value
 	private int mCurrentValue;
@@ -43,7 +67,10 @@ public final class SeekBarPreference extends DialogPreference implements OnSeekB
 		// Read parameters from attributes
 		mMinValue = attrs.getAttributeIntValue(PREFERENCE_NS, ATTR_MIN_VALUE, DEFAULT_MIN_VALUE);
 		mMaxValue = attrs.getAttributeIntValue(PREFERENCE_NS, ATTR_MAX_VALUE, DEFAULT_MAX_VALUE);
+		mStepValue = attrs.getAttributeIntValue(PREFERENCE_NS, ATTR_STEP_VALUE, DEFAULT_STEP_VALUE);
 		mDefaultValue = attrs.getAttributeIntValue(ANDROID_NS, ATTR_DEFAULT_VALUE, DEFAULT_CURRENT_VALUE);
+		// Log.i("SEEKBAR", "minValue=" + mMinValue);
+		// Log.i("SEEKBAR", "maxValue=" + mMaxValue);
 	}
 
 	@Override
@@ -93,14 +120,19 @@ public final class SeekBarPreference extends DialogPreference implements OnSeekB
 	@Override
 	public CharSequence getSummary() {
 		// Format summary string with current value
-		final String summary = super.getSummary().toString();
+		String summary = "";
+		if (super.getSummary() != null) {
+			summary = super.getSummary().toString();
+		}
 		final int value = getPersistedInt(mDefaultValue);
 		return String.format(summary, value);
 	}
 
 	@Override
-	public void onProgressChanged(final SeekBar seek, final int value, final boolean fromTouch) {
+	public void onProgressChanged(final SeekBar seek, int value, final boolean fromTouch) {
 		// Update current value
+		value = (Math.round(value / mStepValue)) * mStepValue;
+		mSeekBar.setProgress(value);
 		mCurrentValue = value + mMinValue;
 		// Update label with current value
 		mValueText.setText(Integer.toString(mCurrentValue));
