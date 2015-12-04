8fcb6703b1c760bcf220a70609c080c7f1e81d24
Oliver <oliver.geith@gmx.net>
2015-12-04 21:53:54 +0100

    New CoolListPreference

---
+++ src/de/geithonline/android/basics/preferences/CoolListPreference.java
@@ -0,0 +1,32 @@
+package de.geithonline.android.basics.preferences;
+
+import android.content.Context;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CoolListPreference extends ListPreference {
+
+	public CoolListPreference(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+			@Override
+			public boolean onPreferenceChange(final Preference preference, final Object newValue) {
+				setSummary("" + newValue);
+				return true;
+			}
+		});
+
+		setSummary(getEntry());
+	}
+
+	@Override
+	protected View onCreateView(final ViewGroup parent) {
+		setSummary(getEntry());
+		return super.onCreateView(parent);
+	}
+
+}
