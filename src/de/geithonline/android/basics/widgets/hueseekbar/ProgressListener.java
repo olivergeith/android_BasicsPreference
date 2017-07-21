3d5eaef390e7168cbf244022297fd9fcf8d43bdc
olivergeith <oliver.geith@gmx.net>
2017-07-21 13:33:13 +0200

    Hue Seekbar

---
+++ src/de/geithonline/android/basics/widgets/hueseekbar/ProgressListener.java
@@ -0,0 +1,10 @@
+
+package de.geithonline.android.basics.widgets.hueseekbar;
+
+/**
+ * Created by mertsimsek on 28/05/2017.
+ */
+
+public interface ProgressListener {
+    void onProgressChange(int progress);
+}
